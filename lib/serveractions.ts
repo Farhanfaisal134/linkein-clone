eda7f2d73df039150e18f3da27543c234beeff8b
Farhan <o3190474834@gmail.com>
2024-06-15 07:03:18 +0500

    few typo changes

--- lib/serveractions.ts
+++ lib/serveractions.ts
@@ -107,7 +107,7 @@ export const createCommentAction = async (postId: string, formData: FormData) =>
       user: userDatabase,
     });
 
-    post.comments?.push(comment._id);
+    post.comments?.push(comment._id as any);
     await post.save();
 
 
